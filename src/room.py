ad3d3f0e92cc27bbe4598f682a24887f1e6cb49e
zambrej <j.zambre66@gmail.com>
2023-01-30 22:06:20 +0800

    Room code implementation

---
+++ src/room.py
@@ -0,0 +1,29 @@
+from src.status import Status
+
+
+class Room:
+    def __init__(self, room_number, status):
+        self.room_number = room_number
+        # status == Status.Available or status == Status.Occupied or status == Status.Vacant
+        self.status = status
+
+    def get_status(self):
+        return self.status
+
+    def set_status(self, status):
+        self.status = status
+
+    def check_in(self):
+        self.set_status(Status.Occupied)
+
+    def check_out(self):
+        self.set_status(Status.Vacant)
+
+    def cleaned(self):
+        self.set_status(Status.Available)
+
+    def repaired(self):
+        self.set_status(Status.Vacant)
+
+    def out_of_service(self):
+        self.set_status(Status.Repair)
---
+++ tests/test_room.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class TestRoomFunctions(unittest.TestCase):
+
+    def test_get_room_status(self):
+        pass
+
+    def test_set_room_status(self):
+        pass
+
+    def test_checkin(self):
+        pass
+
+    def test_checkout(self):
+        pass
+
+    def test_clean(self):
+        pass
+
+    def test_room_marked_out_of_service(self):
+        pass
+
+    def test_room_repaired(self):
+        pass
