3fd9586150ab72c77a336e649b3a5fed3843849e
zambrej <j.zambre66@gmail.com>
2023-01-31 20:13:29 +0800

    cleaned code

--- src/hotel.py
+++ src/hotel.py
@@ -105,31 +105,3 @@ class Hotel:
             room.repaired()
         except ValueError as err:
             return str(err)
-
-
-
-"""
-    room1A = Room("1A", Status.Available)
-    room1B = Room("1B", Status.Available)
-    room1C = Room("1C", Status.Available)
-    room1D = Room("1D", Status.Available)
-    room1E = Room("1E", Status.Available)
-    #floor 2
-    room2A = Room("2A", Status.Available)
-    room2B = Room("2B", Status.Available)
-    room2C = Room("2C", Status.Available)
-    room2D = Room("2D", Status.Available)
-    room2E = Room("2E", Status.Available)
-    #Floor 3
-    room3A = Room("1A", Status.Available)
-    room3B = Room("1A", Status.Available)
-    room3C = Room("1A", Status.Available)
-    room3D = Room("1A", Status.Available)
-    room3E = Room("1A", Status.Available)
-    #Floor 4
-    room4A = Room("1A", Status.Available)
-    room4B = Room("1A", Status.Available)
-    room4C = Room("1A", Status.Available)
-    room4D = Room("1A", Status.Available)
-    room4E = Room("1A", Status.Available)
-    """
